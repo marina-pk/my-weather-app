47034d0832c50acee17ee7cb28cb6c601a484263
Marina <mpyak93@gmail.com>
2021-10-19 15:59:21 -0400

    Integrated the 5-day weather forecast

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
+let dailyParamsCard = `
+        <div class="col-2">
+          <div class="card" id="daily-parameters">
+            <div class="card-header"></div>
+            <div class="card-body">
+              <ul class="list-group list-group-horizontal">
+                <li class="list-group-item">
+                  <i class="fas fa-temperature-low"></i>Low </i>
+                </li>
+              </ul>
+              <ul class="list-group list-group-horizontal-sm">
+                <li class="list-group-item"><i class="fas fa-tint"></i> Humidity </li>
+              </ul>
+              <ul class="list-group list-group-horizontal-md">
+                <li class="list-group-item">
+                  <i class="fas fa-wind"></i>Wind 
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+`;
+
 let now = new Date();
 
 let h6 = document.querySelector("h6#currentDayCard");
@@ -21,8 +44,81 @@ function searchCity(event) {
   let cityLabel = document.querySelector("#currentCity");
   cityLabel.innerHTML = searchInput;
 
+  function formatDay(timestamp) {
+    let date = new Date(timestamp * 1000);
+    let day = date.getDay();
+    let days = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
+
+    return days[day];
+  }
+
+  function displayForecast(response) {
+    console.log(now.getDay());
+    let forecast = response.data.daily;
+
+    let forecastElement = document.querySelector("#forecast");
+
+    let forecastHTML = `<div class="row">`;
+    let forecastHTMLCards = ``;
+    forecast.forEach(function (forecastDay, index) {
+      //let dt = new Date(forecastDay.dt);
+
+      if (index < 6 && index != 0) {
+        forecastHTMLCards =
+          forecastHTMLCards +
+          `
+      <div class="col-2">
+          <div class="card text-center daily-stats-card">
+            <div class="card-header">
+              <h6 class="card-subtitle mb-2 text-muted">${formatDay(
+                forecastDay.dt
+              )}</h6>
+              <img
+                src="http://openweathermap.org/img/wn/${
+                  forecastDay.weather[0].icon
+                }@2x.png"
+                class="card-img-top"
+                alt="weather-icon"
+              />
+              <br />
+              <h3>${Math.round(forecastDay.temp.day)}°</h3>
+              <small>${forecastDay.weather[0].description}</small>
+            </div>
+            <div class="card-body">
+              <ul class="list-group list-group-horizontal">
+                <li class="list-group-item">${Math.round(
+                  forecastDay.temp.min
+                )}°</li>
+              </ul>
+              <ul class="list-group list-group-horizontal-sm">
+                <li class="list-group-item">${forecastDay.humidity}%</li>
+              </ul>
+              <ul class="list-group list-group-horizontal-md">
+                <li class="list-group-item">${Math.round(
+                  forecastDay.wind_speed * 3.6
+                )} km/h</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+  `;
+      }
+    });
+
+    forecastHTML =
+      forecastHTML + dailyParamsCard + forecastHTMLCards + `</div>`;
+    forecastElement.innerHTML = forecastHTML;
+  }
+
   function getForecast(coordinates) {
-    console.log(coordinates);
     let apiKey = "8d9838178b5b401f1b4e7cb5af18e210";
     let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(displayForecast);
